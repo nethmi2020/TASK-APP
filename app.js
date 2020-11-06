b4c25c74d5f1499d482afa814cd4fdfe0f46a191
nethmi2020 <nethmiwelgamvila@gmail.com>
2020-11-06 18:50:24 +0530

    second

--- app.js
+++ app.js
@@ -12,5 +12,34 @@ todoButton.addEventListener('click',addTodo);
 
 //functions
 function addTodo(event){
-    console.log
+    //prevent form from submitting
+  event.preventDefault();
+
+  //TODO div
+const todoDiv=document.createElement("div");
+todoDiv.classList.add("todo");
+//create LI
+const newTodo=document.createElement('li');
+newTodo.innerText='hey';
+newTodo.classList.add('todo-item');
+todoDiv.appendChild(newTodo);
+
+//completed mark button
+
+const completedButton=document.createElement('button');
+completedButton.innerHTML= '<i class="fas fa-check"></i>';
+completedButton.classList.add('complete-btn');
+todoDiv.appendChild(completedButton);
+
+
+//completed trash button
+
+const trashButton=document.createElement('button');
+trashButton.innerHTML= '<i class="fas fa-trash"></i>';
+trashButton.classList.add('trash-btn');
+todoDiv.appendChild(trashButton);
+
+//append to list
+todoList.appendChild(todoDiv);
+
 }
