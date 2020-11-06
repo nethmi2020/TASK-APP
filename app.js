21a79d11514e57905a584b7e6a2862fef8bb4acb
nethmi2020 <nethmiwelgamvila@gmail.com>
2020-11-06 17:23:05 +0530

    first

---
+++ app.js
@@ -0,0 +1,16 @@
+//selectors
+
+const todoInput=document.querySelector('.todo-input');
+const todoButton=document.querySelector('.todo-button');
+const todoList=document.querySelector('.todo-list');
+
+//Event Listeners
+
+todoButton.addEventListener('click',addTodo);
+
+
+
+//functions
+function addTodo(event){
+    console.log
+}
